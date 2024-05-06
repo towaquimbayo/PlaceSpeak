59b56fa38b81855eb856971424a52b08eb9d2241
Towa Quimbayo <towacurtis@hotmail.com>
2024-05-06 12:34:31 -0700

    added AlertMessage component

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import Navbar from "./Navbar";
 
 export default function Layout({ title, isLandingPage = false, children }) {
   if (title && typeof document !== "undefined") {
@@ -12,6 +13,7 @@ export default function Layout({ title, isLandingPage = false, children }) {
         <meta charSet="utf-8" />
         <title>{isLandingPage ? "PlaceSpeak" : `${title} | PlaceSpeak`}</title>
       </Helmet>
+      <Navbar />
       <div id="wrapper">{children}</div>
     </>
   );
