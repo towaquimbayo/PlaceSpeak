5d34f956c5f128f1c1c530c0a4f00b77b265e380
noufilsaqib <noufil.s@hotmail.com>
2024-05-05 17:45:19 -0700

    set up achievements page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
+import Achievements from "./pages/Achievements";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="profile" element={<Profile />} />
+      <Route path="achievements" element={<Achievements />} />
     </Routes>
   );
 }
---
+++ frontend/src/pages/Achievements.js
@@ -0,0 +1,7 @@
+export default function Achievements() {
+    return (
+        <div>
+            <h1>Achievements Page</h1>
+        </div>
+    );
+}
