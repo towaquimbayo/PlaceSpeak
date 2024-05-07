3623bf524f3c3e8ab1e523ef3e32fde2252349c1
Abhishek <abhishekchouhannk@gmail.com>
2024-05-07 11:22:10 -0700

    wrote a simple hello world get request for /hello route

--- backend/pages/views.py
+++ backend/pages/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 # Create your views here.
+
+# '/hello' route (define functions for serving different kinds of requests here)
+class HelloWorldView(APIView):
+  def get(self, request):
+    message = "Hello, world!"
+    return Response(data={'message': message})
