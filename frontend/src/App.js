6c13bb3d315344d8f7233279b401f103249cbb57
noufilsaqib <noufil.s@hotmail.com>
2024-05-07 12:10:12 -0700

    set up login and register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import "./css/global.css";
 import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Profile from "./pages/Profile";
 import Achievements from "./pages/Achievements";
 
@@ -8,6 +10,8 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
       <Route path="profile" element={<Profile />} />
       <Route path="achievements" element={<Achievements />} />
     </Routes>
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+export default function Login() {
+    return (
+        <Layout title="Login">
+            <h1>Login Page</h1>
+        </Layout>
+    );
+}
