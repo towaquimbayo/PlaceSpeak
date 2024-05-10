a20d27d6dd865b99b3f041bff703fb24df95135d
Juan Escalada <juanescalada175@gmail.com>
2024-05-09 17:58:00 -0700

    Add phone verification endpoitn

--- backend/pages/views.py
+++ backend/pages/views.py
@@ -25,3 +25,21 @@ class UpdateEmailVerificationStatus(APIView):
             return Response({'message': 'Email verification status updated successfully'}, status=status.HTTP_200_OK)
         else:
             return Response({'error': 'Verified email status not provided'}, status=status.HTTP_400_BAD_REQUEST)
+
+"""
+UpdatePhoneVerificationStatus class
+
+This class is used to update the phone verification status of a user.
+"""
+class UpdatePhoneVerificationStatus(APIView):
+    def post(self, request, user_id):
+        user = get_object_or_404(User, user_id=user_id)
+        verified_phone = request.data.get('verified_phone')
+        if verified_phone is not None:
+            user.verified_phone = verified_phone
+            user.save()
+            return Response({'message': 'Phone verification status updated successfully'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Verified phone status not provided'}, status=status.HTTP_400_BAD_REQUEST)
+
+
