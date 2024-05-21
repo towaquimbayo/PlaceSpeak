1671ed9457d36767c80ce04bf934c781b32a332d
Juan Escalada <juanescalada175@gmail.com>
2024-05-21 16:43:33 -0700

    Add ConfettiModal component

---
+++ frontend/src/components/ConfettiModal.js
@@ -0,0 +1,44 @@
+import ReactModal from "react-modal"
+import ConfettiExplosion from "react-confetti-explosion"
+
+export const ConfettiModal = ({ isOpen, message }) => {
+  return (
+    <ReactModal
+        isOpen={isOpen}
+        contentLabel="Badge Unlocked"
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+          },
+
+          content: {
+            position: "fixed",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: "2rem",
+            backgroundColor: "#fff",
+            border: "none",
+            borderRadius: "0.5rem",
+            boxShadow: "0 0 1rem rgba(0, 0, 0, 0.5)",
+          },
+        }}
+        closeTimeoutMS={3000}
+      >
+        <h2>{message}</h2>
+        {isOpen && <ConfettiExplosion
+          style={{
+            position: "fixed",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+          duration={3000}
+      />}
+      </ReactModal>
+  )
+}
